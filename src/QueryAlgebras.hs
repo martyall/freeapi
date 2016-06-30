464a6a19d3a09a4ec44a0bbc2ceeb3315f8c4de0
Martin <martin@vfiles.com>
2016-06-30 16:19:33 -0400

    first commit

---
+++ src/QueryAlgebras.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE GADTs, DataKinds, TypeFamilies #-}
+
+module QueryAlgebras where
+
+import Types
+
+--------------------------------------------------------------------------------
+-- | CRUD queries
+--------------------------------------------------------------------------------
+
+data Crudable = UserCrud | MediaCrud | VfileCrud | MediaVfileCrud deriving (Show)
+
+data SCrudable a where
+  SUserCrud       :: SCrudable 'UserCrud
+  SMediaCrud      :: SCrudable 'MediaCrud
+  SVfileCrud      :: SCrudable 'VfileCrud
+  SMediaVfileCrud :: SCrudable 'MediaVfileCrud
+
+type family New (c :: Crudable) :: * where
+  New 'UserCrud = UserNew
+  New 'MediaCrud = MediaNew
+  New 'VfileCrud = VfileNew
+  New 'MediaVfileCrud = MediaVfileNew
+
+type family Base (c :: Crudable) :: * where
+  Base 'UserCrud = UserBase
+  Base 'MediaCrud = MediaBase
+  Base 'VfileCrud = VfileBase
+  Base 'MediaVfileCrud = MediaVfileBase
+
+type family ID (c :: Crudable) :: * where
+  ID 'UserCrud = UserId
+  ID 'MediaCrud = MediaId
+  ID 'VfileCrud = VfileId
+  ID 'MediaVfileCrud = MediaVfileId
+
+data CrudF next :: * where
+  CreateOp :: SCrudable c -> New c -> (Base c -> next) -> CrudF next
+  ReadOp   :: SCrudable c -> ID c -> (Maybe (Base c) -> next) -> CrudF next
+  UpdateOp :: SCrudable c -> Base c -> next -> CrudF next
+  DeleteOp :: SCrudable c -> ID c -> next -> CrudF next
+
+instance Functor CrudF where
+  fmap f (CreateOp c n next) = CreateOp c n $ f . next
+  fmap f (ReadOp c i next) = ReadOp c i $ f . next
+  fmap f (UpdateOp c b next) = UpdateOp c b $ f next
+  fmap f (DeleteOp c i next) = DeleteOp c i $ f next
+
+--------------------------------------------------------------------------------
+-- | Builder queries
+--------------------------------------------------------------------------------
---
+++ src/Types.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Types where
+
+import Data.Text (Text)
+
+
+--------------------------------------------------------------------------------
+-- | User
+--------------------------------------------------------------------------------
+newtype UserId = UserId Integer deriving (Eq, Show)
+
+data UserNew = UserNew { email'     :: Text
+                       , username'  :: Text
+                       } deriving (Eq, Show)
+
+data UserBase = UserBase { userId   :: UserId
+                         , email    :: Text
+                         , username :: Text
+                         } deriving (Eq, Show)
+--------------------------------------------------------------------------------
+-- | Media
+--------------------------------------------------------------------------------
+newtype MediaId = MediaId Integer deriving (Eq, Show)
+
+data MediaNew = MediaNew { mediaCaption'  :: Maybe Caption
+                         , mediaRef'      :: Text
+                         , mediaOwner'    :: UserId
+                         } deriving (Eq, Show)
+
+data MediaBase = MediaBase { mediaId      :: MediaId
+                           , mediaCaption :: Maybe Caption
+                           , mediaRef     :: Text
+                           , mediaOwner   :: UserId
+                           } deriving (Eq, Show)
+
+--------------------------------------------------------------------------------
+-- | Vfile
+--------------------------------------------------------------------------------
+newtype VfileId = VfileId Integer deriving (Eq, Show)
+
+data VfileNew = VfileNew { vfileCaption'  :: Maybe Caption
+                         , vfileOwner'    :: UserBase
+                         } deriving (Eq, Show)
+
+data VfileBase = VfileBase { vfileId      :: VfileId
+                           , vfileCaption :: Maybe Caption
+                           , vfileOwner   :: UserId
+                           } deriving (Eq, Show)
+
+data Vfile = Vfile { vfileBase :: VfileBase
+                   , medias    :: [MediaVfileBase]
+                   } deriving (Eq, Show)
+
+--------------------------------------------------------------------------------
+-- | MediaVfile
+--------------------------------------------------------------------------------
+newtype MediaVfileId = MediaVfileId Integer deriving (Eq, Show)
+
+data MediaVfileNew = MediaVfileNew { mvfCaption'  :: Maybe Caption
+                                   , mvfVfile'    :: Vfile
+                                   } deriving (Eq, Show)
+
+data MediaVfileBase = MediaVfileBase { mvfId      :: MediaVfileId
+                                     , mvfCaption :: Maybe Caption
+                                     } deriving (Eq, Show)
+
+--------------------------------------------------------------------------------
+-- | Comment
+--------------------------------------------------------------------------------
+newtype CommentId = CommentId Integer deriving (Eq, Show)
+
+data CommentNew = CommentNew { commentText' :: Text } deriving (Eq, Show)
+
+data CommentBase = CommentBase { commentId   :: CommentId
+                               , commentText :: Text
+                               } deriving (Eq, Show)
+
+--------------------------------------------------------------------------------
+newtype Caption = Caption Text deriving (Eq, Show)
