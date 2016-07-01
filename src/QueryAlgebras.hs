ab92671935f38bb347b9a787252399260939a85e
Martin Allen <martyall@Martins-Air.fios-router.home>
2016-07-01 14:02:41 -0400

    got comments

--- src/QueryAlgebras.hs
+++ src/QueryAlgebras.hs
@@ -5,14 +5,14 @@ module QueryAlgebras where
 import Types
 
 --------------------------------------------------------------------------------
--- | CRUD queries
+-- | VFCRUD types
 --------------------------------------------------------------------------------
 
-data Crudable = UserCrud
-              | MediaCrud
-              | VfileCrud
-              | MediaVfileCrud
-              | CommentCrud
+data VFCrudable = UserCrud
+                | MediaCrud
+                | VfileCrud
+                | MediaVfileCrud
+                | CommentCrud Comment
                 deriving (Show)
 
 data SCrudable a where
@@ -20,41 +20,53 @@ data SCrudable a where
   SMediaCrud      :: SCrudable 'MediaCrud
   SVfileCrud      :: SCrudable 'VfileCrud
   SMediaVfileCrud :: SCrudable 'MediaVfileCrud
-  SCommentCrud    :: SCrudable 'CommentCrud
+  SCommentCrud    :: SComment com -> SCrudable ('CommentCrud com)
 
-type family New (c :: Crudable) :: * where
-  New 'UserCrud = UserNew
-  New 'MediaCrud = MediaNew
-  New 'VfileCrud = VfileNew
-  New 'MediaVfileCrud = MediaVfileNew
-  New 'CommentCrud = CommentNew
+type family NewData (c :: VFCrudable) :: * where
+  NewData 'UserCrud = UserNew
+  NewData 'MediaCrud = MediaNew
+  NewData 'VfileCrud = VfileNew
+  NewData 'MediaVfileCrud = MediaVfileNew
+  NewData ('CommentCrud com) = CommentNew
 
-type family Base (c :: Crudable) :: * where
-  Base 'UserCrud = UserBase
-  Base 'MediaCrud = MediaBase
-  Base 'VfileCrud = VfileBase
-  Base 'MediaVfileCrud = MediaVfileBase
-  Base 'CommentCrud    = CommentBase
+type family BaseData (c :: VFCrudable) :: * where
+  BaseData 'UserCrud = UserBase
+  BaseData 'MediaCrud = MediaBase 'DB
+  BaseData 'VfileCrud = VfileBase 'DB
+  BaseData 'MediaVfileCrud = MediaVfileBase 'DB
+  BaseData ('CommentCrud com) = CommentBase com
 
-type family ID (c :: Crudable) :: * where
-  ID 'UserCrud = UserId
-  ID 'MediaCrud = MediaId
-  ID 'VfileCrud = VfileId
-  ID 'MediaVfileCrud = MediaVfileId
-  ID 'CommentCrud = CommentId
+type family ReadData (c :: VFCrudable) :: * where
+  ReadData 'UserCrud = UserId
+  ReadData 'MediaCrud = MediaId
+  ReadData 'VfileCrud = VfileId
+  ReadData 'MediaVfileCrud = MVFIdentifier
+  ReadData ('CommentCrud com) = CommentId
 
 data CrudF next :: * where
-  CreateOp :: SCrudable c -> New c -> (Base c -> next) -> CrudF next
-  ReadOp   :: SCrudable c -> ID c -> (Maybe (Base c) -> next) -> CrudF next
-  UpdateOp :: SCrudable c -> Base c -> next -> CrudF next
-  DeleteOp :: SCrudable c -> ID c -> next -> CrudF next
+  CreateOp :: SCrudable c
+           -> NewData c
+           -> (Either VfilesError (BaseData c) -> next)
+           -> CrudF next
+  ReadOp   :: SCrudable c
+           -> ReadData c
+           -> (Either VfilesError (BaseData c) -> next)
+           -> CrudF next
+  UpdateOp :: SCrudable c
+           -> BaseData c
+           -> (Either VfilesError () -> next)
+           -> CrudF next
+  DeleteOp :: SCrudable c
+           -> ReadData c
+           -> (Either VfilesError () -> next)
+           -> CrudF next
 
 instance Functor CrudF where
   fmap f (CreateOp c n next) = CreateOp c n $ f . next
   fmap f (ReadOp c i next) = ReadOp c i $ f . next
-  fmap f (UpdateOp c b next) = UpdateOp c b $ f next
-  fmap f (DeleteOp c i next) = DeleteOp c i $ f next
+  fmap f (UpdateOp c b next) = UpdateOp c b $ f . next
+  fmap f (DeleteOp c i next) = DeleteOp c i $ f . next
 
 --------------------------------------------------------------------------------
--- | Builder queries
+-- | 
 --------------------------------------------------------------------------------
--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,9 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies, GADTs, DataKinds #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Types where
 
@@ -18,6 +23,9 @@ data UserBase = UserBase { userId   :: UserId
                          , email    :: Text
                          , username :: Text
                          } deriving (Eq, Show)
+
+type instance Context UserBase DB = UserId
+type instance Context UserBase None = UserBase
 --------------------------------------------------------------------------------
 -- | Media
 --------------------------------------------------------------------------------
@@ -28,56 +36,107 @@ data MediaNew = MediaNew { mediaCaption'  :: Maybe Caption
                          , mediaOwner'    :: UserId
                          } deriving (Eq, Show)
 
-data MediaBase = MediaBase { mediaId      :: MediaId
-                           , mediaCaption :: Maybe Caption
-                           , mediaRef     :: Text
-                           , mediaOwner   :: UserId
-                           } deriving (Eq, Show)
+data MediaBase r = MediaBase { mediaId      :: MediaId
+                             , mediaCaption :: Maybe Caption
+                             , mediaRef     :: Text
+                             , mediaOwner   :: Context UserBase r
+                             }
+deriving instance Eq (Context UserBase r) => Eq (MediaBase r)
+deriving instance Show (Context UserBase r) => Show (MediaBase r)
 
+type instance Context (MediaBase r) DB = MediaId
+type instance Context (MediaBase r) None = MediaBase None
 --------------------------------------------------------------------------------
 -- | Vfile
 --------------------------------------------------------------------------------
 newtype VfileId = VfileId Integer deriving (Eq, Show)
 
 data VfileNew = VfileNew { vfileCaption'  :: Maybe Caption
-                         , vfileOwner'    :: UserBase
+                         , vfileOwner'    :: UserId
                          } deriving (Eq, Show)
 
-data VfileBase = VfileBase { vfileId      :: VfileId
-                           , vfileCaption :: Maybe Caption
-                           , vfileOwner   :: UserId
-                           } deriving (Eq, Show)
+data VfileBase r = VfileBase { vfileId      :: VfileId
+                             , vfileCaption :: Maybe Caption
+                             , vfileOwner   :: Context UserBase r
+                             }
 
-data Vfile = Vfile { vfileBase :: VfileBase
-                   , medias    :: [MediaVfileBase]
-                   } deriving (Eq, Show)
+deriving instance Eq (Context UserBase r) => Eq (VfileBase r)
+deriving instance Show (Context UserBase r) => Show (VfileBase r)
 
+
+data Vfile r = Vfile { vfileBase :: Context (VfileBase r) r
+                     , medias    :: [Context (MediaVfileBase r) r]
+                     }
+
+deriving instance (Eq (Context (MediaVfileBase r) r), Eq (Context (VfileBase r) r))
+  => Eq (Vfile r)
+deriving instance (Show (Context (MediaVfileBase r) r), Show (Context (VfileBase r) r))
+  => Show (Vfile r)
+
+type instance Context (VfileBase r) DB = VfileId
+type instance Context (VfileBase r) None = VfileBase None
 --------------------------------------------------------------------------------
 -- | MediaVfile
 --------------------------------------------------------------------------------
 newtype MediaVfileId = MediaVfileId Integer deriving (Eq, Show)
 
-data MediaVfileNew = MediaVfileNew { mvfCaption'  :: Maybe Caption
-                                   , mvfVfile'    :: Vfile
+data MediaVfileNew = MediaVfileNew { mvfCaption' :: Maybe Caption
+                                   , mvfMedia'   :: MediaId
+                                   , mvfVfile'   :: VfileId
                                    } deriving (Eq, Show)
 
-data MediaVfileBase = MediaVfileBase { mvfId      :: MediaVfileId
-                                     , mvfCaption :: Maybe Caption
-                                     } deriving (Eq, Show)
+data MediaVfileBase r = MediaVfileBase { mvfId      :: MediaVfileId
+                                       , mvfCaption :: Maybe Caption
+                                       , mvfMedia   :: Context (MediaBase r) r
+                                       , mvfVfile   :: Context (VfileBase r) r
+                                       }
 
+deriving instance (Eq (Context (MediaBase r) r), Eq (Context (VfileBase r) r))
+  => Eq (MediaVfileBase r)
+deriving instance (Show (Context (MediaBase r) r), Show (Context (VfileBase r) r))
+  => Show (MediaVfileBase r)
+
+
+data MVFIdentifier = MVFId MediaVfileId
+                   | MVFPair (MediaId, VfileId)
+                     deriving (Eq, Show)
+
+type instance Context (MediaVfileBase r) DB = MediaVfileId
+type instance Context (MediaVfileBase r) None = MediaVfileBase None
 --------------------------------------------------------------------------------
 -- | Comment
 --------------------------------------------------------------------------------
+data Comment = MediaComment | MediaVfileComment deriving (Eq, Show)
+
 newtype CommentId = CommentId Integer deriving (Eq, Show)
 
 data CommentNew = CommentNew { commentText'  :: Text
                              , commentMedia' :: MediaId
                              } deriving (Eq, Show)
 
-data CommentBase = CommentBase { commentId    :: CommentId
-                               , commentText  :: Text
-                               , commentMedia :: MediaId
-                               } deriving (Eq, Show)
+data CommentBase men = CommentBase { commentId    :: CommentId
+                                   , commentText  :: Text
+                                   , commentItem  :: CommentSourceId men
+                                   }
+
+deriving instance Eq (CommentSourceId men) => Eq (CommentBase men)
+deriving instance Show (CommentSourceId men) => Show (CommentBase men)
+
+type family CommentSourceId (a :: Comment) :: *
+
+type instance CommentSourceId 'MediaComment = MediaId
+type instance CommentSourceId 'MediaVfileComment = MediaVfileId
+
+data SComment c where
+  SMediaComment :: SComment 'MediaComment
+  SMediaVfileComment :: SComment 'MediaVfileComment
 
 --------------------------------------------------------------------------------
 newtype Caption = Caption Text deriving (Eq, Show)
+
+newtype VfilesError = VfilesError Text deriving (Eq, Show)
+
+data ResourceContext = DB | None deriving (Show)
+
+type family Context (a :: *) (r :: ResourceContext) :: *
+
